7981be23fd35c79ce90ff4fa5a06a2ca2560a838
Aman Thakur <uttamthakur2999@gmail.com>
2021-11-20 23:01:37 +0530

    Assigmnet-1

---
+++ block-BNaadz/blog/middlewares/auth.js
@@ -0,0 +1,26 @@
+var User = require('../models/User');
+var Article = require('../models/Article');
+module.exports = {
+  loggedInUser: (req, res, next) => {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      res.redirect('/users/login');
+    }
+  },
+  userInfo: (req, res, next) => {
+    var userId = req.session && req.session.userId;
+    if (userId) {
+      User.findById(userId, 'name email', (err, user) => {
+        if (err) return next(err);
+        req.user = user;
+        req.session.user = user;
+        next();
+      });
+    } else {
+      req.user = null;
+      req.session.user = null;
+      next();
+    }
+  },
+};
